d8fd3f4063962f4ff66cb8657788ec07f12f1f31
Ssalem1235 <107416304+Ssalem1235@users.noreply.github.com>
2026-03-10 16:33:43 -0400

    Update cartoon.js

--- assignments/cartoon.js
+++ assignments/cartoon.js
@@ -1,5 +1,16 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+window.onload = function() {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  //Backgound is skyblue
+  ctx.fillStyle = "skyblue";
+  ctx.fillRect(0, 0, 800, 500);
+}
+
+
+
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
 // Background - sky color
-ctx.fillStyle = "skyblue";
-ctx.fillRect(0, 0, 800, 500);
+// ctx.fillStyle = "skyblue";
+// ctx.fillRect(0, 0, 800, 500);
