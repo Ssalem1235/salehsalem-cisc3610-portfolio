67392b65cfce928af03afa29ea7eedf5090f8d9e
Ssalem1235 <107416304+Ssalem1235@users.noreply.github.com>
2026-03-10 16:23:54 -0400

    Update cartoon.js

--- assignments/cartoon.js
+++ assignments/cartoon.js
@@ -2,4 +2,4 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 // Background - sky color
 ctx.fillStyle = "skyblue";
-ctx.fillRect(0,0,800,500);
+ctx.fillRect(0, 0, 800, 500);
