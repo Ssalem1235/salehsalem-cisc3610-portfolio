f3db9aefad8001e86d3ad890cbc55948b69282bd
Ssalem1235 <107416304+Ssalem1235@users.noreply.github.com>
2026-03-10 16:20:07 -0400

    Update cartoon.js

--- assignments/cartoon.js
+++ assignments/cartoon.js
@@ -1,1 +1,5 @@
-// My cartoon drawing code goes here
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+// Background - sky color
+ctx.fillStyle = "skyblue";
+ctx.fillRect(0,0,800,500);
